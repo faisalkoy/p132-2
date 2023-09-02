ee2a309f31beaa1410ead8ef725fb55ec82fa846
faisalkoy <117185756+faisalkoy@users.noreply.github.com>
2023-09-02 11:13:16 +0400

    Add files via upload

---
+++ bottle.js
@@ -0,0 +1,30 @@
+status="";
+img="";
+objects=[];
+
+function setup() 
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function preload(){
+img = loadImage('bottle.png');
+}
+
+
+function modelLoaded() {
+    console.log("Model Loaded!")
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
+}
